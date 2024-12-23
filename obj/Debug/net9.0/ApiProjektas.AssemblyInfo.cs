12d1def1854c24578fcff9101614a51e0a62b836
Th1nkeR1S <161242488+Th1nkeR1S@users.noreply.github.com>
2024-12-23 07:45:01 +0200

    4

--- obj/Debug/net9.0/ApiProjektas.AssemblyInfo.cs
+++ obj/Debug/net9.0/ApiProjektas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiProjektas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88893c425131a2c7d0e0d62376cec973b3973545")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76aca070314f743e00cae6891bfa84888a4b6383")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiProjektas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiProjektas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
